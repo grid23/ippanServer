470cbbd4a64b845f34a3f491cfe76556e547d54b
Grid23 <hello@grid23.net>
2017-04-10 10:43:52 +0200

    main file update remove archive class file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict"
 
 module.exports = {
-    Server: require("./lib/Server").Server
+    folder: require("./lib/folder").folder
+  , AvailablePort: require("./lib/AvailablePort").AvailablePort
+  , Mime: require("./lib/Mime").Mime
+  , PhantomSandbox: require("./lib/PhantomSandbox").PhantomSandbox
+  , PhantomTask: require("./lib/PhantomTask").PhantomTask
+  , SecureServer: require("./lib/SecureServer").SecureServer
+  , Server: require("./lib/Server").Server
+  , SignalingUpgrade: require("./lib/SignalingUpgrade").SignalingUpgrade
+  , UnixSocket: require("./lib/UnixSocket").UnixSocket
+  , UnixSocketServer: require("./lib/UnixSocketServer").UnixSocketServer
+  , WebSocketUpgrade: require("./lib/WebSocketUpgrade").WebSocketUpgrade
 }
